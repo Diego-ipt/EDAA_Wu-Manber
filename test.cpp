5206ba4e2eb3c5323657be353afd4b887416e9b1
Diego-ipt <diperez2022@udec.cl>
2025-09-30 14:19:55 -0300

    implementacion base

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include "core/wu_manber.cpp"
+using namespace std;
+
+int main()
+{
+
+    // Code
+    string text = "the cat sat on the mat";
+    string pattern = "the";
+
+    WuManber(text, pattern);
+    return 0;
+}
